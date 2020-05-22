ee468a31cb7c7ddb2cdf9a4ddd604d72d78d0cd6
jpgfour <jeremy.grise@gmail.com>
2020-05-21 20:15:53 -0500

    Class 8 Studio - got empty /form up!

--- src/main/java/org/launchcode/skillstracker/controllers/SkillsController.java
+++ src/main/java/org/launchcode/skillstracker/controllers/SkillsController.java
@@ -8,12 +8,11 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @ResponseBody
-//@RequestMapping("hello")
 public class SkillsController {
 
     //return root request
     @GetMapping()
-    public String helloAroundTheWorldForm(){
+    public String plainOldHTML(){
         return "<html>" +
                 "<body>" +
                 "<h1>Skills Tracker</h1>" +
@@ -23,37 +22,47 @@ public class SkillsController {
                 "<li>Pascal</li>" +
                 "<li>Visual Basic</li>" +
                 "</ol>" +
-//                "<form action='/hello' method='get'>" + //submit a request to /hello
-//                "<input type='text' name='name'>" +
-//                //implement dropdown AKA select
-//                "<select name=\"hellos\" id=\"hellos-select\">\n" +
-//                "    <option value=\"\">--Please choose an option--</option>\n" +
-//                "    <option value=\"french\">French</option>\n" +
-//                "    <option value=\"spanish\">Spanish</option>\n" +
-//                "    <option value=\"german\">German</option>\n" +
-//                "    <option value=\"italian\">Italian</option>\n" +
-//                "    <option value=\"portuguese\">Portuguese</option>\n" +
-//                "    <option value=\"hindi\">Hindi</option>\n" +
-//                "    <option value=\"persian-farsi\">Persian (Farsi)</option>\n" +
-//                "    <option value=\"russian\">Russian</option>\n" +
-//                "    <option value=\"japanese\">Japanese</option>\n" +
-//                "    <option value=\"korean\">Korean</option>\n" +
-//                "    <option value=\"turkish\">Turkish</option>\n" +
-//                "    <option value=\"mongolian\">Mongolian</option>\n" +
-//                "    <option value=\"kazakh\">Kazakh</option>\n" +
-//                "    <option value=\"hungarian\">Hungarian</option>\n" +
-//                "    <option value=\"arabic\">Arabic</option>\n" +
-//                "    <option value=\"hausa\">Hausa</option>\n" +
-//                "    <option value=\"swahili\">Swahili</option>\n" +
-//                "    <option value=\"mandarin\">Mandarin</option>\n" +
-//                "    <option value=\"cantonese\">Cantonese</option>\n" +
-//                "    <option value=\"bahasa-indonesia\">Bahasa Indonesia</option>\n" +
-//                "    <option value=\"english\">English</option>\n" +
-//                "</select>" +
-//                "<input type ='submit' value='Greet me!'>" +
-//                "</form>" +
                 "</body>" +
                 "</html>";
+    }
+
+    @GetMapping("form")
+    public String returnForm(){
+        return "<form action='/' method='get'>" +
+                "<label for='name'>Name:</label>" + //submit a request to the root? I hope
+                "<br>" +
+                "<input type='text' name='name'>" +
+                "<br>" +
+                "<label for='fave1'>My favorite language:</label>" +
+                "<br>" +
+                "<select name='fave1' id='fave1'>\\n\" +\n" +
+                "//                \"    <option value=\\\"\\\">--Please choose an option--</option>\\n\" +\n" +
+                "//                \"    <option value=\\\"basic\\\">Basic</option>\\n\" +\n" +
+                "//                \"    <option value=\\\"pascal\\\">Pascal</option>\\n\" +" +
+                "//                \"    <option value=\\\"vb\\\">Visual Basic</option>\\n\" +" +
+                "</select>" +
+                "<br>" +
+                "<label for='fave2'>My second favorite language:</label>" +
+                "<br>" +
+                "<select name='fave2' id='fave2'>\\n\" +\n" +
+                "//                \"    <option value=\\\"\\\">--Please choose an option--</option>\\n\" +\n" +
+                "//                \"    <option value=\\\"basic\\\">Basic</option>\\n\" +\n" +
+                "//                \"    <option value=\\\"pascal\\\">Pascal</option>\\n\" +" +
+                "//                \"    <option value=\\\"vb\\\">Visual Basic</option>\\n\" +" +
+                "</select>" +
+                "<br>" +
+                "<label for='fave3'>My third favorite language:</label>" +
+                "<br>" +
+                "<select name='fave3' id='fave3'>\\n\" +\n" +
+                "//                \"    <option value=\\\"\\\">--Please choose an option--</option>\\n\" +\n" +
+                "//                \"    <option value=\\\"basic\\\">Basic</option>\\n\" +\n" +
+                "//                \"    <option value=\\\"pascal\\\">Pascal</option>\\n\" +" +
+                "//                \"    <option value=\\\"vb\\\">Visual Basic</option>\\n\" +" +
+                "</select>" +
+                "<br>" +
+                "<input type ='submit' value='SUBMIT!'>" +
+                "</form>";
+
     }
 
 
